9ed05a6507a66cd7e9b01c911a90a10ea51c8b95
Anton Kling <anton@kling.gg>
2023-10-22 23:24:02 +0200

    libc: Implement fscanf function

--- userland/libc/stdio/fscanf.c
+++ userland/libc/stdio/fscanf.c
@@ -2,6 +2,9 @@
 #include <assert.h>
 
 int fscanf(FILE *stream, const char *format, ...) {
-    // FIXME
-    assert(0);
+  va_list ap;
+  va_start(ap, format);
+  int rc = vfscanf(stream, format, ap);
+  va_end(ap);
+  return rc;
 }
