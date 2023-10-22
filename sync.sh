4761f12894dcfbbcebca73c2a3af9315eb5b7505
Anton Kling <anton@kling.gg>
2023-10-22 21:31:13 +0200

    Remove junk files from sync.sh script.

--- sync.sh
+++ sync.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-#pwd
-#cd /home/anton/prj/osdev/
-#cd ./userland/json/hashmap
-#make clean
-#make
-#cd ../../..
-#
-#cd ./userland/json
-#make clean
-#make
-#cd ../..
-#
 cd ./userland/libgui
 make clean
 make
@@ -21,35 +9,11 @@ cd ./userland/libc
 make
 make install
 cd ../..
-cd ./userland/nasm-2.16.01
-#make clean
-make
-#make install
-cd ../..
 
-cd ./userland/compiler
-make
-cd ../..
-
-#cd ./userland/init
-#make clean
-#make
-#cd ../..
-#
 cd ./userland/sh
 make clean
 make
 cd ../..
-cd ./userland/sha1sum
-make clean
-make
-cd ../..
-#
-#cd ./userland/cat
-#make clean
-#make
-#cd ../..
-#
 cd ./userland/terminal
 make clean
 make
@@ -80,16 +44,8 @@ make clean
 make
 cd ../..
 
-#cd ./userland/figlet
-#cd ./userland/figlet-2.2.5/
-#make clean
-#make
-#cd ../..
-
 pwd
 sudo mount ext2.img mount
-#sudo cp ./userland/init/init ./mount/init
-#sudo cp ./userland/cat/cat ./mount/cat
 sudo cp ./userland/sh/sh ./mount/sh
 sudo cp ./userland/terminal/term ./mount/term
 sudo cp ./userland/snake/snake ./mount/snake
@@ -97,26 +53,7 @@ sudo cp ./userland/ante/ante ./mount/ante
 sudo cp ./userland/windowserver/ws ./mount/ws
 sudo cp ./userland/test/test ./mount/test
 sudo cp ./userland/minibox/minibox ./mount/minibox
-sudo cp ./userland/nasm-2.16.01/nasm ./mount/nasm
 
-sudo cp ./userland/figlet-2.2.5/figlet ./mount/
-sudo cp ./userland/figlet-2.2.5/chkfont ./mount/
-sudo cp ./userland/figlet-2.2.5/figlist ./mount/
-sudo cp ./userland/figlet-2.2.5/showfigfonts ./mount/
-
-sudo cp ./userland/figlet-2.2.5/fonts/*.flf ./mount/fonts/
-sudo cp ./userland/figlet-2.2.5/fonts/*.flc ./mount/fonts/
-
-sudo cp ./DOOM1.WAD ./mount/DOOM1.WAD
-sudo cp ./userland/compiler/compiler ./mount/cc
-sudo cp ./userland/sha1sum/sha1sum ./mount/sha1sum
-sudo cp ./userland/sha1sum/sha1sum ./mount/sa
-sudo cp ./userland/fasm/fasm ./mount/fasm
-sudo cp ./userland/ed/ed ./mount/ed
-sudo cp ./userland/SmallerC/smlrc ./mount/sc
-sudo cp ./userland/libc/crt0.o ./mount/crt0.o
-sudo cp ./userland/libc/libc.a ./mount/libc.a
-sudo cp ./userland/doomgeneric/doomgeneric/doomgeneric ./mount/doomgeneric
 echo -e "int main(void) {\nprintf(\"hi\");\nreturn 0;\n}" > /tmp/main.c
 
 cd ./mount
