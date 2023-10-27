8eede5fd642db2c56a1f5185973815fbd2b0afa4
Anton Kling <anton@kling.gg>
2023-10-27 16:32:13 +0200

    LibC: Optimize memcpy function
    Instead of copying byte by byte it now copies in 64, 32, 16, 8 bit
    chunks.

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,10 +1,19 @@
 #include "../include/string.h"
 
-void *__attribute__((optimize("O0")))
+void *
 memcpy(void *dest, const void *src, uint32_t n) {
   unsigned char *d = dest;
   const unsigned char *s = src;
-  //	for(;n--;) *d++ = *s++;
+
+  for (; n >= 8; n -= 8, d += 8, s += 8)
+    *(uint64_t *)d = *(uint64_t *)s;
+
+  for (; n >= 4; n -= 4, d += 4, s += 4)
+    *(uint32_t *)d = *(uint32_t *)s;
+
+  for (; n >= 2; n -= 2, d += 2, s += 2)
+    *(uint16_t *)d = *(uint16_t *)s;
+
   for (; n; n--)
     *d++ = *s++;
   return dest;
