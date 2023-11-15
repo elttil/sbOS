7c115f97f16ba1d26adf9b477520002cedc05c09
Anton Kling <anton@kling.gg>
2023-11-15 23:06:05 +0100

    Add untracked files

---
+++ kernel/crypto/xoshiro256plusplus/xoshiro256plusplus.c
@@ -0,0 +1,46 @@
+/*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+#include <stdint.h>
+#include <string.h>
+
+/* This is xoshiro256++ 1.0, one of our all-purpose, rock-solid generators.
+   It has excellent (sub-ns) speed, a state (256 bits) that is large
+   enough for any parallel application, and it passes all tests we are
+   aware of.
+
+   For generating just floating-point numbers, xoshiro256+ is even faster.
+
+   The state must be seeded so that it is not everywhere zero. If you have
+   a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+   output to fill s. */
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+  return (x << k) | (x >> (64 - k));
+}
+
+static uint64_t s[4];
+
+void seed_xoshiro_256_pp(uint64_t input[4]) { memcpy(s, input, sizeof(s)); }
+
+uint64_t xoshiro_256_pp(void) {
+  const uint64_t result = rotl(s[0] + s[3], 23) + s[0];
+
+  const uint64_t t = s[1] << 17;
+
+  s[2] ^= s[0];
+  s[3] ^= s[1];
+  s[1] ^= s[2];
+  s[0] ^= s[3];
+
+  s[2] ^= t;
+
+  s[3] = rotl(s[3], 45);
+
+  return result;
+}
---
+++ kernel/crypto/xoshiro256plusplus/xoshiro256plusplus.h
@@ -0,0 +1,2 @@
+void seed_xoshiro_256_pp(uint64_t input[4]);
+uint64_t xoshiro_256_pp(void);
