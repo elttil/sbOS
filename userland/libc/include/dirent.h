b353808b13db85abbbc0c3331c04c778620ed017
Anton Kling <anton@kling.gg>
2023-11-20 00:41:29 +0100

    LibC: Add macros for file regnition

--- userland/libc/include/dirent.h
+++ userland/libc/include/dirent.h
@@ -7,6 +7,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define STAT_REG 0
+#define STAT_DIR 1
+
+#define S_ISREG(_m) (_m & (1 << STAT_REG))
+#define S_ISDIR(_m) (_m & (1 << STAT_DIR))
+
 struct dirent {
   ino_t d_ino;           // File serial number.
   char d_name[PATH_MAX]; // Filename string of entry.
