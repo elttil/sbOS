c99d6608825c02783a58cddd930794cd0d35ab03
Anton Kling <anton@kling.gg>
2024-05-05 16:20:26 +0200

    LibC: fflush() should not dereference stream if it is NULL

--- userland/libc/stdio/fflush.c
+++ userland/libc/stdio/fflush.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <syscall.h>
+#include <errno.h>
 
 // https://pubs.opengroup.org/onlinepubs/9699919799/functions/fflush.html
 int fflush(FILE *stream) {
-  if (stream->fflush)
-    stream->fflush(stream);
-  return 0;
+  if (stream) {
+    if (stream->fflush) {
+      stream->fflush(stream);
+      return 0;
+    }
+  }
+  errno = ENXIO;
+  return -1;
 }
