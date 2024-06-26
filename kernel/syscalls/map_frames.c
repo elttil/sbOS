2e2f89c7508dede2c970e6061c2f66d4536162ca
Anton Kling <anton@kling.gg>
2024-06-26 21:24:22 +0200

    Kernel: Remove unused syscalls

--- kernel/syscalls/map_frames.c
+++
@@ -1,6 +0,0 @@
-#include <mmu.h>
-#include <syscalls.h>
-
-u32 syscall_map_frames(u32 address, u32 size) {
-  return (u32)mmu_map_user_frames((void *)address, size);
-}
--- kernel/syscalls/virtual_to_physical.c
+++
@@ -1,6 +0,0 @@
-#include <mmu.h>
-#include <syscalls.h>
-
-u32 syscall_virtual_to_physical(u32 virtual) {
-  return (u32)virtual_to_physical((void *)virtual, NULL);
-}
