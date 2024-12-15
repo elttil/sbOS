3f8324fde2128c3308f957891262d3372fb57ebf
Anton Kling <anton@kling.gg>
2024-12-15 19:20:04 +0100

    libc: Fix undefined behavior for non positive VLA
    Apparently a VLA of zero size is undefined behavior.

--- userland/libc/string/memmove.c
+++ userland/libc/string/memmove.c
@@ -3,6 +3,9 @@
 // copy bytes in memory with overlapping areas
 // https://pubs.opengroup.org/onlinepubs/9699919799/functions/memmove.html
 void *memmove(void *s1, const void *s2, size_t n) {
+  if (0 == n) {
+    return s1;
+  }
   // Copying takes place as if the n bytes from the object pointed to by s2 are
   // first copied into a temporary array of n bytes that does not overlap the
   // objects pointed to by s1 and s2, and then the n bytes from the temporary
