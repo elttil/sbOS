2cee944915064953ba2a4d76cb8ecf0a98889702
Anton Kling <anton@kling.gg>
2025-01-04 21:06:25 +0100

    libc: Fix out bounds shift

--- userland/libc/arpa/inet/htonl.c
+++ userland/libc/arpa/inet/htonl.c
@@ -5,7 +5,7 @@
 uint32_t htonl(uint32_t hostlong) {
 #if BYTE_ORDER == LITTLE_ENDIAN
   hostlong = (uint32_t)(htons(hostlong >> 16)) |
-             (uint32_t)(htons(hostlong & 0xFFFF) << 16);
+             (((uint32_t)htons(hostlong & 0xFFFF)) << 16);
 #endif
   return hostlong;
 }
